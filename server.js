f46cff53064f5907ba8bbe7a498d815c58d8726a
kuzugr <monmon.syusyo@gmail.com>
2018-09-25 23:04:39 +0900

    test

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/dist'));
-console.log('1')
-console.log(__dirname + '/dist')
 app.listen(process.env.PORT || 8080);
 const path = require('path');
+console.log('kokokara')
 app.get('/*', function(req, res){
+  console.log(res);
   res.sendFile(path.join(__dirname + '/dist/index.html'));
-  console.log(path.join(__dirname + '/dist/index.html'));
 });
+console.log('kokomade')
 console.log('Server listening on port 8080');
